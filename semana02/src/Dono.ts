7506e9ddb4e58783e5305e3592aec0a700df2d3b
GERMANO RODRIGUES GOMES <germano_gomes@estudante.sesisenai.org.br>
2024-08-26 21:53:07 -0300

    Semana02 Aula 26/08

---
+++ semana02/src/Dono.ts
@@ -0,0 +1,11 @@
+
+export default class Dono {
+    constructor(nome: string, telefone: string){
+        this.nome = nome;
+        this.telefone = telefone;
+
+    }
+    
+    nome: string;
+    telefone: string;
+}
---
+++ semana02/src/index.ts
@@ -0,0 +1,11 @@
+import Cachorro from "./Cachorro";
+import Dono from "./Dono";
+
+var d1= new Dono("joca","(11) 0000-000");
+
+var c1 = new Cachorro("juca","Vira-lata","Caramelo","Médio",d1);
+
+
+c1.exibirCacchorro();
+c1.exibirDono()
+
